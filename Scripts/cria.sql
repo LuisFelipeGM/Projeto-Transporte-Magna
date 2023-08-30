2384dbe3ff58b64cbc7ed9c2116aa7ade5dabef2
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-08-30 16:20:18 -0300

    Atualização do Script de Cria e Apaga

--- Scripts/cria.sql
+++ Scripts/cria.sql
@@ -1,116 +1,120 @@
 create schema transporte;
 
 -- CRIANDO TABELA USUARIO
-create table if not exists transporte.USUARIO(
+create table if not exists transporte.T_TPU_USUARIO(
 	ID_USUARIO serial primary key,
-	NOME_USUARIO varchar(255) not null CHECK (NOME_USUARIO <> ''),
-	CPF varchar(11) not null CHECK (NOME_USUARIO <> ''),
+	NOME_USUARIO varchar(255) not null CHECK (NOME_USUARIO <> '')
+);
+
+-- CRIANDO TABELA FUNCIONARIO
+create table if not exists transporte.T_TPU_FUNCIONARIO(
+	ID_FUNCIONARIO serial primary key,
+	CODIGO integer not null,
+	FUNCAO varchar(255) not null check (FUNCAO <> ''),
+	SALARIO numeric(10,2) not null default 1320.00,
+	INICIO_HORA_TRABALHO time not null,
+	FIM_HORA_TRABALHO time not null,
+	ID_USUARIO integer not null references transporte.T_TPU_USUARIO(ID_USUARIO) on delete cascade
+);
+
+-- CRIANDO TABELA PASSAGEIRO
+create table if not exists transporte.T_TPU_PASSAGEIRO(
+	ID_PASSAGEIRO serial primary key,
+	CPF integer not null,
 	DATA_NASCIMENTO date not null,
+	ID_USUARIO integer not null references transporte.T_TPU_USUARIO(ID_USUARIO) on delete cascade,
 	unique(CPF)
 );
 
 
 -- CRIANDO TABELA CARTAO
 create type transporte.tipo_passageiro as enum('PASSE ESCOLAR', 'PASSE SENIOR', 'VALE TRANSPORTE', 'PASSE LIVRE', 'PASSE ESPECIAL');
-create table if not exists transporte.CARTAO(
+create table if not exists transporte.T_TPU_CARTAO(
 	ID_CARTAO serial primary key,
-	NUMERO_CARTAO integer not null,
+	NUMERO integer not null,
 	TIPO_PASSAGEIRO transporte.tipo_passageiro not null,
 	DATA_ATIVACAO date not null default now()::date,
 	DATA_VALIDADE date not null default (now() + interval '1 year')::date,
 	SALDO numeric(10,2) not null default 0.00,
-	ID_USUARIO integer not null references transporte.USUARIO(ID_USUARIO) on delete cascade,
-	unique(NUMERO_CARTAO)
+	ID_PASSAGEIRO integer not null references transporte.T_TPU_PASSAGEIRO(ID_PASSAGEIRO) on delete cascade,
+	unique(NUMERO)
+);
+
+-- CRIANDO TABELA COMPRA_BILHETE
+create table if not exists transporte.T_TPU_COMPRA_BILHETE(
+	ID_BILHETE serial primary key,
+	CODIGO UUID not null default gen_random_uuid(),
+	DATA_GERACAO date not null default now()::date,
+	UTILIZADO boolean not null default false
+);
+
+-- CRIANDO TABELA TIPO_PASSAGEM
+create table if not exists transporte.T_TPU_TIPO_PASSAGEM(
+	ID_TIPO_PASSAGEM serial primary key,
+	ID_CARTAO integer references transporte.T_TPU_PASSAGEIRO(ID_PASSAGEIRO) on delete cascade,
+	ID_BILHETE integer references transporte.T_TPU_COMPRA_BILHETE(ID_BILHETE) on delete cascade
+	CONSTRAINT UM_TIPO CHECK (
+        (ID_BILHETE IS NOT NULL AND ID_CARTAO IS NULL) OR
+        (ID_BILHETE IS NULL AND ID_CARTAO IS NOT NULL)
+    )
 );
 
 
 -- CRIANDO TABELA CIDADE
 create type transporte.zona_sp as enum('ZONA SUL', 'ZONA NORTE', 'ZONA LESTE', 'ZONA OESTE');
-create table if not exists transporte.CIDADE(
+create table if not exists transporte.T_TPU_CIDADE(
 	ID_CIDADE serial primary key,
-	NOME_CIDADE varchar(255) not null CHECK (NOME_CIDADE <> ''),
+	NOME varchar(255) not null CHECK (NOME <> ''),
 	NUMERO_POPULACIONAL integer not null,
 	ZONA_SP transporte.zona_sp not null
 );
 
 
 -- CRIANDO TABELA LINHA
-create table if not exists transporte.LINHA(
+create table if not exists transporte.T_TPU_LINHA(
 	ID_LINHA serial primary key,
-	NOME_LINHA varchar(255) not null CHECK (NOME_LINHA <> ''),
-	NUMERO_LINHA integer not null,
-	CONEXAO_LINHA boolean not null default false,
-	unique(NUMERO_LINHA)
-);
-
-
--- CRIANDO TABELA FEEDBACK
-create table if not exists transporte.FEEDBACK(
-	ID_FEEDBACK serial primary key,
-	COMENTARIO varchar(500) not null CHECK (COMENTARIO <> ''),
-	NOTA numeric(2,1) not null,
-	DATA_FEEDBACK date not null default now()::date,
-	ID_LINHA integer not null references transporte.LINHA(ID_LINHA) on delete cascade,
-	ID_USUARIO integer not null references transporte.USUARIO(ID_USUARIO) on delete cascade
+	NOME varchar(255) not null CHECK (NOME <> ''),
+	NUMERO integer not null,
+	CONEXAO boolean not null default false,
+	unique(NUMERO)
 );
 
 
 -- CRIANDO TABELA ESTACAO
-create table if not exists transporte.ESTACAO(
+create table if not exists transporte.T_TPU_ESTACAO(
 	ID_ESTACAO serial primary key,
-	NOME_ESTACAO varchar(255) not null check (NOME_ESTACAO <> ''),
+	NOME varchar(255) not null check (NOME <> ''),
 	HORARIO_ABERTURA time not null default '04:00'::time,
 	HORARIO_FECHAMENTO time not null default '00:00'::time,
-	ID_CIDADE integer not null references transporte.CIDADE(ID_CIDADE) on delete cascade,
-	ID_LINHA integer not null references transporte.LINHA(ID_LINHA) on delete cascade
+	ID_CIDADE integer not null references transporte.T_TPU_CIDADE(ID_CIDADE) on delete cascade,
+	ID_LINHA integer not null references transporte.T_TPU_LINHA(ID_LINHA) on delete cascade
 );
 
 
--- CRIANDO TABELA CARTAO ESTACAO
-create table if not exists transporte.CARTAO_ESTACAO(
-	ID_CARTAO_ESTACAO serial primary key,
+-- CRIANDO TABELA VIAGEM
+create table if not exists transporte.T_TPU_VIAGEM(
+	ID_VIAGEM serial primary key,
 	PRECO numeric(5,2) not null default 4.20,
-	ID_CARTAO integer not null references transporte.CARTAO(ID_CARTAO) on delete cascade,
-	ID_ESTACAO integer not null references transporte.ESTACAO(ID_ESTACAO) on delete cascade
+	ID_TIPO_PASSAGEM integer not null references transporte.T_TPU_TIPO_PASSAGEM(ID_TIPO_PASSAGEM) on delete cascade,
+	ID_ESTACAO integer not null references transporte.T_TPU_ESTACAO(ID_ESTACAO) on delete cascade
 );
 
 
 -- CRIANDO TABELA TREM
-create table if not exists transporte.TREM(
+create table if not exists transporte.T_TPU_TREM(
 	ID_TREM serial primary key,
 	CODIGO_IDENTIFICACAO varchar(10) not null check (CODIGO_IDENTIFICACAO <> ''),
 	CAPACIDADE_PASSAGEIROS integer not null,
 	ESTADO_MANUTENCAO boolean not null default false,
-	ID_LINHA integer not null references transporte.LINHA(ID_LINHA) on delete cascade
+	ID_LINHA integer not null references transporte.T_TPU_LINHA(ID_LINHA) on delete cascade
 );
 
 
--- CRIANDO TABELA FUNCIONARIO
-create table if not exists transporte.FUNCIONARIO(
-	ID_FUNCIONARIO serial primary key,
-	NOME_COMPLETO varchar(255) not null check (NOME_COMPLETO <> ''),
-	FUNCAO varchar(255) not null check (NOME_COMPLETO <> ''),
-	INICIO_HORA_TRABALHO time not null,
-	FIM_HORA_TRABALHO time not null,
-	SALARIO numeric(10,2) not null default 1320.00
-);
 
-
--- CRIANDO TABELA FUNCIONARIO_TREM
-create table if not exists transporte.FUNCIONARIO_TREM(
-	ID_FUNCIONARIO_TREM serial primary key,
+-- CRIANDO TABELA CONDUCAO
+create table if not exists transporte.T_TPU_CONDUCAO(
+	ID_CONDUCAO serial primary key,
 	DATA_UTILIZADO date not null default now()::date,
-	ID_FUNCIONARIO integer not null references transporte.FUNCIONARIO(ID_FUNCIONARIO) on delete cascade,
-	ID_TREM integer not null references transporte.TREM(ID_TREM) on delete cascade
+	ID_FUNCIONARIO integer not null references transporte.T_TPU_FUNCIONARIO(ID_FUNCIONARIO) on delete cascade,
+	ID_TREM integer not null references transporte.T_TPU_TREM(ID_TREM) on delete cascade
 );
-
-
--- CRIANDO TABELA MANUTENCAO
-create table if not exists transporte.MANUTENCAO(
-	ID_MANUTENCAO serial primary key,
-	DATA_INICIO date not null default now()::date,
-	DATA_FIM date,
-	ID_FUNCIONARIO integer not null references transporte.FUNCIONARIO(ID_FUNCIONARIO) on delete cascade,
-	ID_TREM integer not null references transporte.TREM(ID_TREM) on delete cascade
-);
-

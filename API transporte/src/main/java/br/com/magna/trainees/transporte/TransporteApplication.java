73f1065c846c979d5fbe5ffa6289c5a3010adb6e
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-09-15 15:33:07 -0300

    Add commit inicial API SpringBoot

---
+++ API transporte/src/main/java/br/com/magna/trainees/transporte/TransporteApplication.java
@@ -0,0 +1,13 @@
+package br.com.magna.trainees.transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteApplication.class, args);
+	}
+
+}

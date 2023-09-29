4fe93ba886dc37335c18590cd81a3969a64201d3
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-09-29 20:11:49 -0300

    Arrumando Relacionamentos para pesquisa no Postman

--- API transporte/src/main/java/br/com/magna/trainees/transporte/models/ConexaoModel.java
+++ API transporte/src/main/java/br/com/magna/trainees/transporte/models/ConexaoModel.java
@@ -1,5 +1,6 @@
 package br.com.magna.trainees.transporte.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -16,6 +17,7 @@ public class ConexaoModel {
 
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "FK_ESTACAO", foreignKey = @ForeignKey(name = "FK_ESTACAO_CONEXAO"))
+    @JsonIgnore
     private EstacaoModel estacao;
 
     public Long getId() {

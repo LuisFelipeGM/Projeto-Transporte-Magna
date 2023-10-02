80ec77e89ccc4df5ccf5bbd7a0babfcafc9c3fe3
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2023-10-02 16:49:00 -0300

    Atualização na carga de dados das conexões

--- Versão - 2/Scripts/carga de dados endereco.sql
+++ Versão - 2/Scripts/carga de dados endereco.sql
@@ -1,4 +1,3 @@
--- Dados na tabela LINHA 
 insert into transporte.TB_LINHA(NOME, NUMERO) values
 	('Azul', 1),
 	('Verde', 2),
@@ -14,166 +13,535 @@ insert into transporte.TB_LINHA(NOME, NUMERO) values
 	('Jade', 13),
 	('Prata', 15);
 
--- select * from transporte.TB_LINHA;
-
-
--- Dados na tabela ESTACAO
+-- Inserindo as ESTAÇÕES da LINHA AZUL
 insert into transporte.TB_ESTACAO(NOME) values
 	('Tucuruvi'),
+	('Padara Inglesa'),
+	('Jardim São Paulo - Ayrton Senna'),
 	('Santana'),
-	('Tietê'),
+	('Carandiru'),
+	('Portuguesa - Tietê'),
+	('Armênia'),
+	('Tiradentes'),
 	('Luz'),
+	('São Bento'),
 	('Sé'),
-	('Liberdade'),
+	('Japão - Liberdade'),
+	('São Joaquim'),
+	('Vergueiro'),
 	('Paraíso'),
+	('Ana Rosa'),
 	('Vila Mariana'),
-	('Jabaquara'),
-	('Paulista'),
-	('Triaton - Masp'),
+	('Santa Cruz'),
+	('Praça da Árvore'),
+	('Saúde'),
+	('São Judas'),
+	('Conceição'),
+	('Jabaquara');
+
+-- Inserindo as ESTAÇÕES da LINHA VERDE
+insert into transporte.TB_ESTACAO(NOME) values
+	('Vila Madalena'),
+	('Sumaré'),
+	('Clínicas'),
+	('Paulista - Consolação'),
+	('Trtiaton - Masp'),
+	('Brigadeiro'),
+	('Chácara Kablin'),
+	('Santos - Imigrantes'),
 	('Alto do Ipiranga'),
-	('Barra Funda'),
-	('República'),
-	('Anhangabaú'),
-	('Pedro II'),
+	('Sacomã'),
+	('Tamanduateí'),
+	('Vila Prudente');
+
+
+-- Inserindo as ESTAÇÕES da LINHA VERMELHA
+insert into transporte.TB_ESTACAO(NOME) values
+	('Corinthians - Itaquera'),
+	('Artur Alvim'),
+	('Patriarca - Vila Ré'),
+	('Guilhermina Esperança'),
+	('Vila Matilde'),
 	('Penha'),
-	('Itaquera'),
-	('Vila Sônia'),
-	('Morumbi'),
-	('Butantã'),
-	('Faria Lima'),
-	('Mackenzie'),
-	('Capão Redondo'),
-	('Brooklin'),
-	('Moema'),
-	('Lapa'),
-	('Jaraguá'),
-	('Perus'),
-	('Botujuru'),
-	('Jundiaí'),
-	('Itapevi'),
-	('Barueri'),
-	('Carapicuiba'),
-	('Osasco'),
-	('Barra Funda'),
-	('Ceasa'),
-	('Pinheiros'),
-	('Vila Olímpia'),
-	('Morumbi'),
-	('Mooca'),
-	('Ipiranga'),
-	('Utinga'),
-	('Capuava'),
-	('Rio Grande da Serra'),
-	('Luz'),
+	('Carrão'),
+	('Tatuapé'),
+	('Belém'),
+	('Bresser - Mooca'),
 	('Brás'),
+	('Pedro II'),
+	('Anhangabaú'),
+	('República'),
+	('Santa Cecília'),
+	('Mal. Deodoro'),
+	('Palmeiras - Barra Funda');
+
+
+
+	-- Inserindo as ESTAÇÕES da LINHA AMARELA
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Vila Sônia'),
+	('São Paulo - Morumbi'),
+	('Butantã'),
+	('Pinheiros'),
+	('Faria Lima'),
+	('Fradique Coutinho'),
+	('Oscar Freire'),
+	('Higienópolis - Mackenzie');
+
+
+-- Inserindo as ESTAÇÕES da LINHA LILÁS
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Capão Redondo'),
+	('Campo Limpo'),
+	('Vila das Belezas'),
+	('Giovanni Gronchi'),
+	('Santo Amaro'),
+	('Largo Treze'),
+	('Adolfo Pinheiro'),
+	('Alto da Boa Vista'),
+	('Borba Gato'),
+	('Brooklin'),
+	('Campo Belo'),
+	('Eucaliptos'),
+	('Moema'),
+	('AACD - Servidor'),
+	('Hospital São Paulo');
+
+
+	-- Inserindo as ESTAÇÕES da LINHA RUBI
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Jundiaí'),
+	('Várzea Paulista'),
+	('Campo Limpo Paulista'),
+	('Botujuru'),
+	('Francisco Morato'),
+	('Baltazar Fidélis'),
+	('Franco da Rocha'),
+	('Caieiras'),
+	('Perus'),
+	('Vila Aurora'),
+	('Jaraguá'),
+	('Vila Clarice'),
+	('Pirituba'),
+	('Piqueri'),
+	('Lapa'),
+	('Água Branca');
+
+
+	-- Inserindo as ESTAÇÕES da LINHA DIAMANTE
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Amador Bueno'),
+	('Santa Rita'),
+	('Itapevi'),
+	('Eng. Cardoso'),
+	('Sagrado Coração'),
+	('Jandira'),
+	('Jardim Silveira'),
+	('Barueri'),
+	('Antônio João'),
+	('Santa Terezinha'),
+	('Carapicuíba'),
+	('Gen. Miguel Costa'),
+	('Quitaúna'),
+	('Com. Sampaio'),
+	('Osasco'),
+	('Pres. Altino'),
+	('Imperatriz Leopoldina'),
+	('Domingo de Moraes'),
+	('Lapa'),
+	('Júlio Prestes');
+
+
+	-- Inserindo as ESTAÇÕES da LINHA ESMERALDA
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Ceasa'),
+	('Vila-Lobos - Jaguaré'),
+	('Cidade Universitária'),
+	('Hebraica Rebouças'),
+	('Cidade Jardim'),
+	('Vila Olímpia'),
+	('Berrini'),
+	('Morumbi'),
+	('Granja Julieta'),
+	('João Dias'),
+	('Socorro'),
+	('Jurubatuba'),
+	('Autódromo'),
+	('Primavera - Interlagos'),
+	('Grajaú'),
+	('Mendes - Vila Natal');
+
+
+-- Inserindo as ESTAÇÕES da LINHA TURQUESA
+	insert into transporte.TB_ESTACAO(NOME) values
+	('Juventus - Mooca'),
+	('Ipiranga'),
+	('São Caetano do Sul'),
+	('Utinga'),
+	('Prefeito Saladino'),
+	('Santo Ándre'),
+	('Capuava'),
+	('Mauá'),
+	('Guapituba'),
+	('Ribeirão Pires'),
+	('Rio Grande da Serra');
+
+
+-- Inserindo as ESTAÇÕES da LINHA CORAL
+insert into transporte.TB_ESTACAO(NOME) values
+	('Dom Bosco'),
+	('José Bonifácio'),
 	('Guaianases'),
+	('Antônio Gianetti Neto'),
+	('Ferraz de Vasconcelos'),
 	('Poá'),
+	('Calmon Viana'),
+	('Suzano'),
+	('Jundiapeba'),
+	('Braz Cubas'),
 	('Mogi das Cruzes'),
+	('Estudantes');
+
+
+-- Inserindo as ESTAÇÕES da LINHA SAFIRA
+insert into transporte.TB_ESTACAO(NOME) values
+	('Eng. Goulart'),
 	('USP Leste'),
+	('Comendador Ermelino'),
+	('São Miguel Paulista'),
+	('Jd. Helena - Vila Mara'),
 	('Itaim Paulista'),
 	('Jardim Romano'),
+	('Eng. Manuel Feio'),
 	('Itaquaquecetuba'),
-	('Calmon Viana'),
-	('Eng Goulart'),
+	('Aracaré');
+
+
+-- Inserindo as ESTAÇÕES da LINHA JADE
+insert into transporte.TB_ESTACAO(NOME) values
 	('Guarulhos - Cecap'),
-	('Aeroporto Guarulhos'),
-	('Vila Prudente'),
-	('Oratório'),
+	('Aeroporto - Guarulhos');
+
+
+-- Inserindo as ESTAÇÕES da LINHA PRATA
+insert into transporte.TB_ESTACAO(NOME) values
+	('Jardim Colonial'),
+	('São Mateus'),
+	('Fazenda da Juta'),
+	('Sapopemba'),
+	('Jd. Planalto'),
 	('Vila União'),
-	('Jardim Colonial');
+	('Vila Tolstói'),
+	('Camilo Haddad'),
+	('São Lucas'),
+	('Oratório'),
+	('Vila Prudente');
 
--- select * from transporte.TB_ESTACAO;
+select * from transporte.tb_estacao;
 
-
--- Dados na tabela CONEXAO
-insert into transporte.TB_CONEXAO (FK_LINHA, FK_ESTACAO) values
+-- CONEXÕES
+-- Linha Azul
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
 	(1, 1),
 	(1, 2),
-	(2, 3),
-	(2, 4),
-	(3, 5),
-	(3, 6),
-	(4, 7),
-	(4, 8),
-	(5, 9),
-	(6, 10),
-	(6, 11),
-	(7, 12),
-	(7, 13),
-	(8, 14),
-	(8, 15),
-	(9, 16),
-	(9, 17),
-	(10, 18),
-	(10, 19),
-	(11, 20),
-	(11, 21),
-	(12, 22),
-	(12, 23),
-	(13, 24),
-	(13, 25);
+	(1, 3),
+	(1, 4),
+	(1, 5),
+	(1, 6),
+	(1, 7),
+	(1, 8),
+
+	-- Conexões Luz
+	(1, 9), -- Azul
+	(4, 9), -- Amarela
+	(6, 9), -- Rubi
+	(9, 9), -- Turquesa
+
+	(1, 10),
+
+	-- Conexões Sé
+	(1, 11), -- Azul
+	(3, 11), -- Vermelha
+
+	(1, 12),
+	(1, 13),
+	(1, 14),
+
+	-- Conexões Paraíso
+	(1, 15), -- Azul
+	(2, 15), -- Verde
+
+	-- Conexões Ana Rosa
+	(1, 16), -- Azul
+	(2, 16), -- Verde
+
+	(1, 17),
+
+	-- Conexões Santa Cruz
+	(1, 18), -- Azul
+	(5, 18), -- Lilás
+
+	(1, 19),
+	(1, 20),
+	(1, 21),
+	(1, 22),
+	(1, 23);
 
 
-insert into transporte.TB_ESTADO(NOME, SIGLA) values
-	('Acre', 'AC'),
-	('Amapá', 'AP'),
-	('Amazonas', 'AM'),
-	('Pará', 'PA'),
-	('Rondônia', 'RO'),
-	('Roraima', 'RR'),
-	('Tocantins', 'TO'),
-	('Alagoas', 'AL'),
-	('Bahia', 'BA'),
-	('Ceará', 'CE'),
-	('Maranhão', 'MA'),
-	('Paraíba', 'PB'),
-	('Pernambuco', 'PE'),
-	('Piauí', 'PI'),
-	('Rio Grande do Norte', 'RN'),
-	('Sergipe', 'SE'),
-	('Espírito Santo', 'ES'),
-	('Minas Gerais', 'MG'),
-	('Rio de Janeiro', 'RJ'),
-	('São Paulo', 'SP'),
-	('Paraná', 'PR'),
-	('Rio Grande do Sul', 'RS'),
-	('Santa Catarina', 'SC'),
-	('Distrito Federal', 'DF'),
-	('Goiás', 'GO'),
-	('Mato Grosso', 'MT'),
-	('Mato Grosso do Sul', 'MS');
+-- Conexões Linha Verde
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(2, 24),
+	(2, 25),
+	(2, 26),
 
--- select * from transporte.TB_ESTADO;
+	-- Conexões Paulista
+	(2, 27), -- Verde
+	(4, 27), -- Amarelo
+
+	(2, 28),
+	(2, 29),
+
+	-- Conexões Chácara Klabin
+	(2, 30), -- Verde
+	(5, 30), -- Lilás
+
+	(2, 31),
+	(2, 32),
+	(2, 33),
+
+	-- Conexões Tamanduateí
+	(2, 34), -- Verde
+	(9, 34), -- Turquesa
+
+	-- Conexões Vila Prudente
+	(2, 35), -- Verde
+	(13, 35); -- Prata
+
+-- Conexões Linha Vermelha
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	-- Conexões Itaquera
+	(3, 36), -- Vermelha
+	(10, 36), -- Coral
+
+	(3, 37),
+	(3, 38),
+	(3, 39),
+	(3, 40),
+	(3, 41),
+	(3, 42),
+
+	-- Conexões Tatuapé
+	(3, 43), -- Vermelha
+	(10, 43), -- Coral
+	(11, 43), -- Safira
+
+	(3, 44),
+	(3, 45),
+
+	-- Conexões Brás
+	(3, 46), -- Vermelha
+	(9, 46), -- Turquesa
+	(10, 46), -- Coral
+	(11, 46), -- Safira
+
+	(3, 47),
+	(3, 48),
+
+	-- Conexões Republica
+	(3, 49), -- Vermelha
+	(4, 49), -- Amarela
+
+	(3, 50),
+	(3, 51),
+
+	-- Conexões Barra Funda
+	(3, 52), -- Vermelha
+	(7, 52), -- Diamante
+	(6, 52); -- Rubi
 
 
---Dados na tabela CIDADE
-insert into transporte.TB_CIDADE(NOME, FK_ESTADO) values
-	('Itapevi', 20),
-	('Jandira', 20),
-	('Barueri', 20),
-	('Carapicuíba', 20),
-	('Osasco', 20),
-	('São Paulo', 20),
-	('Caieiras', 20),
-	('Franco da Rocha', 20),
-	('Francisco Morato', 20),
-	('Campo Limpo Paulista', 20),
-	('Várzea Paulista', 20),
-	('Jundiaí', 20),
-	('Guarulhos', 20),
-	('Itapevi', 20),
-	('Itaquaquecetuba', 20),
-	('Mogi das Cruzes', 20),
-	('Suzano', 20),
-	('São Caetano do Sul', 20),
-	('Santo André', 20),
-	('Mauá', 20),
-	('Ribeirão Pires', 20),
-	('Rio Grande da Serra', 20);
--- select * from transporte.TB_ESTADO;
+-- Conexões Linha Amarela
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(4, 53),
+	(4, 54),
+	(4, 55),
+
+	-- Conexões Pinheiros
+	(4, 56), -- Amarela
+	(8, 56), -- Esmeralda
+
+	(4, 57),
+	(4, 58),
+	(4, 59),
+	(4, 60);
+
+
+-- Conexões Linha Lilás
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(5, 61),
+	(5, 62),
+	(5, 63),
+	(5, 64),
+
+	-- Conexões Santo Amaro
+	(5, 65), -- Lilás
+	(8, 65), -- Esmeralda
+
+	(5, 66),
+	(5, 67),
+	(5, 68),
+	(5, 69),
+	(5, 70),
+	(5, 71),
+	(5, 72),
+	(5, 73),
+	(5, 74),
+	(5, 75);
+
+
+-- Conexões Linha Rubi
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(6, 76),
+	(6, 77),
+	(6, 78),
+	(6, 79),
+	(6, 80),
+	(6, 81),
+	(6, 82),
+	(6, 83),
+	(6, 84),
+	(6, 85),
+	(6, 86),
+	(6, 87),
+	(6, 88),
+	(6, 89),
+	(6, 90),
+	(6, 91);
 
 
 
+-- Conexões Linha Diamante
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(7, 92),
+	(7, 93),
+	(7, 94),
+	(7, 95),
+	(7, 96),
+	(7, 97),
+	(7, 98),
+	(7, 99),
+	(7, 100),
+	(7, 101),
+	(7, 102),
+	(7, 103),
+	(7, 104),
+	(7, 105),
 
-	
-	
+	-- Conexões Osasco
+	(7, 106), -- Diamante
+	(8, 106), -- Esmeralda
+
+	-- Conexões Pres. Altino
+	(7, 107), -- Diamante
+	(8, 107), -- Esmeralda
+
+	(7, 108),
+	(7, 109),
+	(7, 110),
+	(7, 111);
+
+
+-- Conexões Linha Esmeralda
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(8, 112),
+	(8, 113),
+	(8, 114),
+	(8, 115),
+	(8, 116),
+	(8, 117),
+	(8, 118),
+	(8, 119),
+	(8, 120),
+	(8, 121),
+	(8, 122),
+	(8, 123),
+	(8, 124),
+	(8, 125),
+	(8, 126),
+	(8, 127);
+
+
+
+-- Conexões Linha Turquesa
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(9, 128),
+	(9, 129),
+	(9, 130),
+	(9, 131),
+	(9, 132),
+	(9, 133),
+	(9, 134),
+	(9, 135),
+	(9, 136),
+	(9, 137),
+	(9, 138);
+
+
+-- Conexões Linha Coral
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(10, 139),
+	(10, 140),
+	(10, 141),
+	(10, 142),
+	(10, 143),
+	(10, 144),
+
+	-- Conexões Calmon Viana
+	(10, 145), -- Coral
+	(11, 145), -- Safira
+
+	(10, 146),
+	(10, 147),
+	(10, 148),
+	(10, 149),
+	(10, 150);
+
+
+-- Conexões Linha Safira
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	-- Conexões Eng. Goulart
+	(11, 151), -- Safira
+	(12, 151), -- Jade
+
+	(11, 152),
+	(11, 153),
+	(11, 154),
+	(11, 155),
+	(11, 156),
+	(11, 157),
+	(11, 158),
+	(11, 159),
+	(11, 160);
+
+
+-- Conexões Linha Jade
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(12, 161),
+	(12, 162);
+
+
+-- Conexões Linha Prata
+insert into transporte.TB_CONEXAO(FK_LINHA, FK_ESTACAO) values
+	(13, 163),
+	(13, 164),
+	(13, 165),
+	(13, 166),
+	(13, 167),
+	(13, 168),
+	(13, 169),
+	(13, 170),
+	(13, 171),
+	(13, 172),
+	(13, 173);
+
+
